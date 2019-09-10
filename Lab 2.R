ab455c76d3365ba334f683546ca599ebdd875506
c00305146 <lauren.walling1@louisiana.edu>
2019-09-09 20:51:10 -0500

    Initialize

---
+++ Lab 2.R
@@ -0,0 +1,46 @@
+a<-7
+8->b
+c="art"
+d=as.integer(2)
+e=5.7
+a%%b
+a%/%b
+b%/%a
+b^e
+#%% is the modulator or tells you the remainder after division
+#%/% is the symbol used to integer division
+#^ is the symbol used for an exponent
+f=c(2:11)
+f
+g={h=4;i=26;j=14}
+h
+i
+j
+h*i+j
+h*(i+j)
+#the 2 equations have two different outputs because of the parenthesis. Without the parenthesis in the 2nd equation they would be the same.
+#With the parenthesis the addition is done first which is then multiplied and withouth the parenthesis the multiplication is done first and then the addition.
+#Inf means infinity
+5^23008437
+#-Inf means negative infinity
+-(5^23008437)
+#NaN means not a number
+0/0
+#NULL is used to get rid of a line in a data set
+#NA means that data is missing from a data set or it is not in the proper form for that column or row of data
+'%LW%'=function(a,b)(4*a+b^2)
+3 %LW% 2
+2%% 3
+4%/%5
+6^7
+switchero.if.then=function(x){
+  if(x=="a")
+    "Whale"
+  else if(x=="b")
+    "Hippo"
+  else if(x=="c")
+    "Lion"
+  else
+    "Cheeta"
+  }
+switchero.if.then("b")

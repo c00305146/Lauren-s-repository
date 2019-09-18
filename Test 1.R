a0bbcb85503d1693d1c896d7bb2029d55c12ba09
c00305146 <lauren.walling1@louisiana.edu>
2019-09-18 14:49:30 -0500

    added script 1 Operators

---
+++ Test 1.R
@@ -0,0 +1,36 @@
+#Test 1
+a=5:14
+a[c(1,7)]
+b=5
+e=11
+b>e
+b<e
+b>=e
+a[c(2,6,9)]
+x=2
+y=6
+z=9
+((z+x)*(z+y))/2
+10*(x-y)
+#The "R" operator for not is a !
+b%%e
+#%% is the modulator or the remainder
+b^e
+#is an exponent
+b%/%e
+#%/% is doing intergal division
+'%myop%'=function(x,y){2/x+3*y}
+7%myop%2
+i<-26
+j=50;k=23;l=5
+j
+k
+l
+#Inf means infinity while -Inf means negaite infinity
+57^1029
+-(57^1029)
+#NaN stands for not a number 
+0/0
+#NA is a placement for a number that is not known
+length(a)=12
+a
